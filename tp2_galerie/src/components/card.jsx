343eac1f09cd310c0f1f3b7e4a9b7137c3475000
Raphael_Doizi <doizi.raphael@gmail.com>
2025-09-16 14:21:09 +0200

    final push design

--- tp2_galerie/src/components/card.jsx
+++ tp2_galerie/src/components/card.jsx
@@ -10,11 +10,11 @@ export default function
     Card({ url, title, categories, author }) {
     return (
         <div className='card'>
-            <h2 className='card-title'>{title}</h2>
-            <p className='card-author'>{author}</p>
             <div className='card-image-wrapper'>
                 <img className='card-image' src={url} alt="" />
             </div>
+            <h2 className='card-title'>{title}</h2>
+            <p className='card-author'>{author}</p>
             <div className='card-categories'>
                 {categories.map((category, index) => (
                     <span key={index} className='card-category'>
